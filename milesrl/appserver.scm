e43743e03956b7d78c4acdfa48acf17758f9fd07
uberblah <soccermiles@hotmail.com>
2015-07-01 17:55:40 -0600

    gitignore, beginnings of module

---
+++ milesrl/appserver.scm
@@ -0,0 +1,10 @@
+
+(define-module (milesrl appserver))
+
+(define (make-appserver index methods) '())
+
+(export make-appserver)
+
+(define (run-appserver server root addr port) '())
+
+(export run-appserver)
